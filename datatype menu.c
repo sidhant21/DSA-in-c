9eeac509a46e519a385db59c6355efaa44317ed4
Sidhant Sharma <153753973+sidhant21@users.noreply.github.com>
2026-02-06 12:58:52 +0530

    Add menu options for list operations in C

---
+++ datatype menu.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    while(1){
+        int ch;
+        printf("1. Insert in Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert at any Position\n");
+        printf("4. Delete from Begginning \n");
+        printf("5. Delete from End \n");
+        printf("6. Delete from given Position\n");
+        printf("7. Display List\n");
+        printf("8. Search Element in List\n");
+        printf("9. Update Element in List\n");
+        printf("10. Sort Element in List\n");
+        printf("11 Exit");
+        printf("Enter Your Choice:\n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: 
+                break;
+            case 2: 
+                break;
+            case 3: 
+                break;
+            case 4: 
+                break;
+            case 5: 
+                break;
+            case 6: 
+                break;
+            case 7: 
+                break;
+            case 8: 
+                break;
+            case 9: 
+                break;
+            case 10: 
+                break;
+            case 11: 
+                break;
+            default:
+                printf("Invalid Choice!!!\n")
+        }
+    }
+    return 0;
+}
