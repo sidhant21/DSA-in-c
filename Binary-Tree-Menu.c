8a0b421e0cfa384335fde7a0afb18e00e1d9380c
Sidhant Sharma <153753973+sidhant21@users.noreply.github.com>
2026-04-09 10:39:15 +0530

    Implement binary tree menu structure

---
+++ Binary-Tree-Menu.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define max 100
+int tree[max];
+
+int main(){
+    int choice,value;
+    while(1){
+        printf("\n***Binary Tree Menu***\n");
+        printf("1. Insert \n");
+        printf("2. Level Order Traversal\n");
+        printf("3. Pre-Order Traversal\n");
+        printf("4. In-Order Traversal\n");
+        printf("5. Post-Order Traversal\n");
+        printf("6. Exit\n");
+        printf("Enter Choice: ");
+        switch(choice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("\nInvalid Choice!!!\n");
+
+        }
+    }
+}
